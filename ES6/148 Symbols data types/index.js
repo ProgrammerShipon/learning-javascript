ee2f5e165856b3e026ca1e5b351c073901e604ad
Shipon Hossen Raju <msshipon234@gmail.com>
2022-11-04 12:18:49 +0600

    Symbol data types , Defrent Defernt data type

---
+++ ES6/148 Symbols data types/index.js
@@ -0,0 +1,16 @@
+console.clear();
+
+let s1 = Symbol();
+let s2 = Symbol('test symbol');
+
+console.log(s1)
+console.log(s2)
+console.log(s1===s2)
+
+let toss = {
+  HEAD: Symbol('HEAD'),
+  TAIL: Symbol('TAIL')
+}
+console.log(toss.HEAD)
+
+
