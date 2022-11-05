e8e8907cb9a043755bffb326095141b66278dd41
Shipon Hossen Raju <msshipon234@gmail.com>
2022-11-05 08:47:11 +0600

    create Iterator Function

---
+++ ES6_2_fifteen2/150-Iterator-Basics/index.js
@@ -0,0 +1,32 @@
+console.clear();
+
+const arr = [1, 2, 3, 4]
+
+// for (let i = 0; i < arr.length; i++ ) {
+//   console.log(arr[i]);
+// }
+
+function createIterator ( collection ) {
+  let i = 0;
+  return {
+    next () {
+      return {
+        done: i >= collection.length,
+        value: collection[i++]
+      }
+    }
+  }
+}
+let iterate = createIterator(arr);
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.dir(iterate.next());
+
+
+console.log(arr[Symbol.iterator])
+
+
+
